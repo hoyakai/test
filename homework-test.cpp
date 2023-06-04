bfa55ef4cfa57e947540738fe4dc09a213013642
hoyakai <112096245+hoyakai@users.noreply.github.com>
2023-06-04 15:55:07 +0900

    homework-test.cpp

---
+++ homework-test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n,i,d[1000]={0,},a=0;
+    pair<int,int> p[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&p.first,&p.second);
+        if(d[i]==0)
+        {
+            d[i]=1;
+            a++;
+        }
+    }
+    sort(p,p+n);
+    for(i=1;i<=n;i++)
+    {
+        
+    }
+}
+// 13904 2457 2873
